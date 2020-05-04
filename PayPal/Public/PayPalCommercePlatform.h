b5ab86539d06aef6c57eb908c3e30161757f9db6
Susan Stevens <susan.stevens@getbraintree.com>
2020-05-04 16:46:52 -0500

    Update SDK's version number to new PayPal name.
    Signed-off-by: Samantha Cannillo <samantha.cannillo@getbraintree.com>

--- PayPal/Public/PayPalCommercePlatform.h
+++ PayPal/Public/PayPalCommercePlatform.h
@@ -1,10 +1,10 @@
 #import <Foundation/Foundation.h>
 
-//! Project version number for PayPalCommerce.
-FOUNDATION_EXPORT double PayPalCommerceVersionNumber;
+/// Project version number for PayPalCommerce.
+FOUNDATION_EXPORT double PayPalVersionNumber;
 
-//! Project version string for PayPalCommerce.
-FOUNDATION_EXPORT const unsigned char PayPalCommerceVersionString[];
+/// Project version string for PayPalCommerce.
+FOUNDATION_EXPORT const unsigned char PayPalVersionString[];
 
 #import "PPCValidatorClient.h"
 #import "PPCValidatorResult.h"
